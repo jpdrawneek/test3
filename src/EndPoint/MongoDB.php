cc22a1ab03e76701ef75fa3b0b91bde9ee985920
John-Paul Drawneek <jpd@drawneek.co.uk>
2016-05-31 18:46:04 +0100

    Basic MongoDB integration

--- src/EndPoint/MongoDB.php
+++ src/EndPoint/MongoDB.php
@@ -2,11 +2,38 @@
 
 namespace MergemarketTest\EndPoint;
 
+use MergemarketTest\Data\TickerCode as TickerCode;
+
 /**
  * Description of MongoDB
  *
  * @author jpd
  */
-class MongoDB {
-  //put your code here
+class MongoDB implements TickerEndPoint {
+  /** @var \MongoDB\Client */
+  protected $connection;
+  /** @var \MongoDB\Database */
+  protected $db;
+  /** @var \MongoDB\Collection */
+  protected $collection;
+
+  public function __construct($uri, $collection) {
+    list($scheme, $blank, $server, $database) = split('/', $uri);
+    if (!(strlen($database) > 0)) {
+      throw new Exception('Database not set in configuration uri');
+    }
+    $this->connection = new \MongoDB\Client($uri);
+    $this->db = $this->connection->$database;
+    $this->collection = $this->db->$collection;
+  }
+
+
+  public function getItems() {
+    $output = [];
+    foreach ($this->collection->find() AS $data) {
+      $output[] = new TickerCode($data['company'], $data['TickerCode']);
+    }
+    return $output;
+  }
+
 }
---
+++ src/EndPoint/TickerEndPoint.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MergemarketTest\EndPoint;
+
+/**
+ *
+ * @author jpd
+ */
+interface TickerEndPoint {
+  
+  public function getItems();
+}
